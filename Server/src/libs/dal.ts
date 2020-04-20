c449e5814020219d58ae8163417d54a6050433f0
Mike <zhendongtt@gmail.com>
2020-04-20 10:47:42 -0400

    add TS code

---
+++ Server/src/libs/dal.ts
@@ -0,0 +1,32 @@
+"use strict";
+/*
+* FILENAME      : dal.ts
+* CREATED BY    : Zhendong Tang
+* MODIFIED BY	: N/A
+* CREATION DATE : April 20, 2020
+* DESCRIPTION   :
+*   This class is for access database
+*/
+
+module.exports = class dalClass {
+    //private Status: typeof Status;
+    /*=========================================================================
+    MethodName: Constructor
+    Description: this status will be updated by Position Object
+    Arguments: string
+    Return Value: none
+    ===========================================================================*/
+    private db:any;
+    constructor() {  
+        this.db=require(`./database`)
+    }
+    //
+    public async GetWord(){
+        let word:any=undefined;
+        word = await this.db.query(`Select * from Class`);
+        return word;
+
+
+    }
+
+};
